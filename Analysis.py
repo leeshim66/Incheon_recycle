b76404dfeb986e67de6b37141bfd94e33883689b
LJY <leeshim66@naver.com>
2022-01-05 17:19:01 +0900

    종속변수별 GridSearch를 통한 xgboost parameter setting 및 예측

--- Analysis.py
+++ Analysis.py
@@ -20,7 +20,8 @@ import sys
 import warnings
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
+from xgboost import XGBRegressor
 
 # sys.setrecursionlimit(10**6)
 warnings.filterwarnings('ignore')
@@ -42,12 +43,20 @@ y2 = data['mean_value']
 y3 = data['real_mean_value']
 
 # random forest 변수중요도 시각화
-rf = RandomForestRegressor()
-rf.fit(X,y1)
-rf_pred = rf.predict(X)
+rf1 = RandomForestRegressor()
+rf1.fit(X,y1)
+rf1_pred = rf1.predict(X)
+
+rf2 = RandomForestRegressor()
+rf2.fit(X,y2)
+rf2_pred = rf2.predict(X)
+
+rf3 = RandomForestRegressor()
+rf3.fit(X,y3)
+rf3_pred = rf3.predict(X)
 
 features = X.columns.values
-a,b = (list(a) for a in zip(*sorted(zip(rf.feature_importances_, features), reverse=False)))
+a,b = (list(a) for a in zip(*sorted(zip(rf1.feature_importances_, features), reverse=False)))
 
 trace = go.Bar(x=a, y=b, marker=dict(color=a, colorscale='Viridis', reversescale=True),
                name = 'RF feature importance', orientation='h')
@@ -57,4 +66,54 @@ layout = dict(width=800, height=500,
 fig = go.Figure(data=[trace])
 fig['layout'].update(layout)
 py.iplot(fig, filename='plotss')
+
+
+# Gridsearch를 통한 파라미터 설정
+param_grid = {
+    'learning_rate' : [0.001,0.005,0.01,0.05],
+    'booster' : ['gbtree', 'gblinear'],
+    'n_estimators': [100, 150, 200],
+    'max_depth': [1,2,3],
+    'min_samples_split': [0,1,2],
+    'min_samples_leaf' : [0,1,2],
+    'max_leaf_node' : [1,2,3]
+}
+
+a1,b1 = (list(a) for a in zip(*sorted(zip(rf1.feature_importances_, features), reverse=True)))
+a2,b2 = (list(a) for a in zip(*sorted(zip(rf2.feature_importances_, features), reverse=True)))
+a3,b3 = (list(a) for a in zip(*sorted(zip(rf3.feature_importances_, features), reverse=True)))
+feature1 = b1[:6]
+feature2 = b2[:6]
+feature3 = b3[:6]
+X1 = X[feature1]# RF변수중요도로 변수 선택
+X2 = X[feature2]
+X3 = X[feature3]
+
+xgb = XGBRegressor()
+
+grid_search1 = GridSearchCV(estimator=xgb, param_grid=param_grid, cv=10, n_jobs=-1)
+grid_search2 = GridSearchCV(estimator=xgb, param_grid=param_grid, cv=10, n_jobs=-1)
+grid_search3 = GridSearchCV(estimator=xgb, param_grid=param_grid, cv=10, n_jobs=-1)
+
+grid_search1.fit(X1,y1)
+grid_search2.fit(X2,y2)
+grid_search3.fit(X3,y3)
+
+# 셀별 y 예측
+XGB1 = XGBRegressor(booster='gbtree', learning_rate=0.01, max_depth=1, n_estimators=200, max_leaf_node=2)
+XGB2 = XGBRegressor(booster='gbtree', learning_rate=0.005, max_depth=1, n_estimators=150, max_leaf_node=2)
+XGB3 = XGBRegressor(booster='gbtree', learning_rate=0.005, max_depth=2, n_estimators=150, max_leaf_node=2)
+XGB1.fit(X1,y1)
+XGB2.fit(X2,y2)
+XGB3.fit(X3,y3)
+
+predict_df = pd.read_csv('data/final/pred_df_211213.csv')
+
+X_pred1 = predict_df[feature1]
+X_pred2 = predict_df[feature2]
+X_pred3 = predict_df[feature3]
+
+XGB_pred1 = XGB1.predict(X_pred1)
+XGB_pred2 = XGB2.predict(X_pred2)
+XGB_pred3 = XGB3.predict(X_pred3)
 
